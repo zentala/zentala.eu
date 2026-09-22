de190fdf69321feff97f8feead5710e4f74a1c01
zentala <zentala@users.noreply.github.com>
2026-09-22 01:02:30 +0000

    deploy: e4d6b142ac99a5e2bfc4ffd46e7fe7fc1e3d8804

---
+++ _astro/hoisted.Ct_W4M-8.js
@@ -0,0 +1,1 @@
+import"./hoisted.OTi2GCp8.js";document.addEventListener("DOMContentLoaded",()=>{const t=document.querySelector('button[onclick^="navigator.clipboard"]');t&&t.addEventListener("click",()=>{navigator.clipboard.writeText("https://zentala.eu"),t.textContent="Copied!",setTimeout(()=>{t.textContent="Copy"},2e3)})});
